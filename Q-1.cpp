dbe2c298fbf9e57c003dc2d42ba956669820eb02
Ritikkumar81 <bcawork81@gmail.com>
2025-11-11 17:15:29 +0530

    Add program to calculate string length

---
+++ Q-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str[100];
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+    int length = strlen(str);
+    cout << "The length of the string is: " << length << endl;
+    return 0;
+}
