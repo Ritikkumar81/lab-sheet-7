31abceefde7bf481214142a4122c6f66892f55b0
Ritikkumar81 <bcawork81@gmail.com>
2025-11-11 20:01:50 +0530

    Add palindrome checker in Q-11.cpp

---
+++ Q-11.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, j, isPalindrome = 1;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    i = 0;
+    j = strlen(str) - 1;
+
+    while(i < j) {
+        if(str[i] != str[j]) {
+            isPalindrome = 0;
+            break;
+        }
+        i++;
+        j--;
+    }
+
+    if(isPalindrome)
+        printf("The string is a palindrome.\n");
+    else
+        printf("The string is not a palindrome.\n");
+
+    return 0;
+}
