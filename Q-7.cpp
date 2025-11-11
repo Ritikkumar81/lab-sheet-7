879bac2230d265239f0fbb85ed61b994c2d26bcd
Ritikkumar81 <bcawork81@gmail.com>
2025-11-11 17:30:13 +0530

    Add string comparison program in Q-7.cpp

---
+++ Q-7.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str1[100], str2[100];
+    cout << "Enter first string: ";
+    cin.getline(str1, 100);
+    cout << "Enter second string: ";
+    cin.getline(str2, 100);
+
+    int result = strcmp(str1, str2);
+
+    if(result == 0)
+        cout << "Both strings are equal." << endl;
+    else if(result < 0)
+        cout << "First string is less than second string." << endl;
+    else
+        cout << "First string is greater than second string." << endl;
+
+    return 0;
+}
